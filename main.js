0b40bf6cce95b0cc8dce39d02a925ba120d93199
Saeem Osman <saeem.osman@gmail.com>
2018-11-28 21:42:24 +0600

    JS file has been modified

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ document.getElementById("issueInputForm").addEventListener("submit", saveIssue);
 function saveIssue(e) {
   var issueDesc = document.getElementById("issueDescInput").value;
   var issueSeverity = document.getElementById("issueSeverityInput").value;
-  var issueAssignedTo = document.getElementById("issueAssignToInput").value;
+  var issueAssignedTo = document.getElementById("issueAssignedToInput").value;
   var issueId = chance.guid();
-  var issueStatus = "open";
+  var issueStatus = "Open";
 
   var issue = {
-    is: issueId,
+    id: issueId,
     description: issueDesc,
     severity: issueSeverity,
     assignedTo: issueAssignedTo,
@@ -17,6 +17,10 @@ function saveIssue(e) {
 
   if (localStorage.getItem("issues") == null) {
     var issues = [];
+    issues.push(issue);
+    localStorage.setItem("issues", JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem("issues"));
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   }
@@ -28,9 +32,37 @@ function saveIssue(e) {
   e.preventDefault();
 }
 
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem("issues"));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
+    }
+  }
+
+  localStorage.setItem("issues", JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem("issues"));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem("issues", JSON.stringify(issues));
+
+  fetchIssues();
+}
+
 function fetchIssues() {
   var issues = JSON.parse(localStorage.getItem("issues"));
-  var issuesList = document.getElementById("issuesList");
+  var issuesListe = document.getElementById("issuesList");
 
   issuesList.innerHTML = "";
 
@@ -42,26 +74,28 @@ function fetchIssues() {
     var status = issues[i].status;
 
     issuesList.innerHTML +=
-      '<siv class="well>' +
-      "<h6>Issue ID:" +
+      '<div class="well">' +
+      "<h6>Issue ID: " +
       id +
       "</h6>" +
-      '<p><span class="label-info' +
+      '<p><span class="label label-info">' +
       status +
-      "</span></p" +
-      _;
-    "<h3>" +
+      "</span></p>" +
+      "<h3>" +
       desc +
       "</h3>" +
-      '<p><span class = "glyphicon glyphicon-time"></span>' +
+      '<p><span class="glyphicon glyphicon-time"></span> ' +
       severity +
-      "</p>";
-    '<p><span class = "glyphicon glyphicon-user"></span>' + assignedTo + "</p>";
-    '<a href="#" onclick = "setStatusClosed(\'' +
+      "</p>" +
+      '<p><span class="glyphicon glyphicon-user"></span> ' +
+      assignedTo +
+      "</p>" +
+      '<a href="#" onclick="setStatusClosed(\'' +
       id +
-      '\')" class = "btn btn-warning">Close</a>';
-    '<a href="#" onclide="deleteIssue(\'' +
+      '\')" class="btn btn-warning">Close</a> ' +
+      '<a href="#" onclick="deleteIssue(\'' +
       id +
-      '\'))" class = "btn btn-danger">Close</a>';
+      '\')" class="btn btn-danger">Delete</a>' +
+      "</div>";
   }
 }
